4d5f25c171f97d843c3b442949cd35b5493e6f1e
EmanueleMelini <meliniemanuele@gmail.com>
2024-12-02 12:36:34 +0100

    Day 1 completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,51 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val pair = parse1(input)
+
+        pair.first.sort()
+        pair.second.sort()
+
+        var sum = 0
+        for ((index, _) in pair.first.withIndex()) {
+            sum += abs(pair.first[index] - pair.second[index])
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        val pair = parse1(input)
+
+        var sim = 0
+        for (lf in pair.first) {
+            sim += (lf * pair.second.stream().filter {
+                it == lf
+            }.count().toInt())
+        }
+
+        return sim
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+
+}
+
+private fun parse1(inputData: List<String>): Pair<MutableList<Int>, MutableList<Int>> {
+
+    val left: MutableList<Int> = ArrayList()
+    val right: MutableList<Int> = ArrayList()
+
+    for (line in inputData) {
+        val data = line.split("   ")
+        left.add(data[0].toInt())
+        right.add(data[1].toInt())
+    }
+
+    return Pair(left, right)
 }
