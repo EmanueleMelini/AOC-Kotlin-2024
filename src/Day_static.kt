9a39e26376a9dd4e7775d9835f33d5f2cd8a7ebd
EmanueleMelini <meliniemanuele@gmail.com>
2024-12-03 10:32:44 +0100

    Added static template for days

---
+++ src/Day_static.kt
@@ -0,0 +1,21 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+
+        return 0
+    }
+
+    val input = readInput("Day_")
+    part1(input).println()
+    part2(input).println()
+
+}
+
+private fun parse(inputData: List<String>): Any {
+
+    return 0
+}
